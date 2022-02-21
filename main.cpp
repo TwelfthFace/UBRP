579cd051fed8cf4bcecde5bac08db2f758716b4b
Jack <alcomeho@gmail.com>
2022-02-21 17:50:16 +0000

    added debug string

--- main.cpp
+++ main.cpp
@@ -10,6 +10,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    cout << "HELLO" << endl;
+    //cout << "HELLO" << endl;
     return a.exec();
 }
